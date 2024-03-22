6b25858ec2065b9601e704f9ceebd8991b9a6ea7
counter185 <33550839+counter185@users.noreply.github.com>
2024-03-22 07:47:31 +0100

    script to copy all dlls

---
+++ copy_devlibs.bat
@@ -0,0 +1,8 @@
+mkdir x64\Debug
+mkdir x64\Release
+mkdir Debug
+mkdir Release
+copy freesprite\devlibs\lib\x64\*.dll x64\Debug
+copy freesprite\devlibs\lib\x64\*.dll x64\Release
+copy freesprite\devlibs\lib\x86\*.dll Debug
+copy freesprite\devlibs\lib\x86\*.dll Release
