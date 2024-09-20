cf0e50b170ae7f98f73b882272c7f20900016185
counter185 <33550839+counter185@users.noreply.github.com>
2024-09-20 11:44:18 +0200

    fix linux build

--- gcc-build/build_linux.sh
+++ gcc-build/build_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir build
-g++-13 ../freesprite/*.cpp ../freesprite/pugixml/*.cpp ../freesprite/zip/*.c ../freesprite/easybmp/*.cpp ../freesprite/astc_dec/*.cpp -I../freesprite/liblcf -I../freesprite/liblcf/generated  -std=c++20 -Wl,-rpath,. -Wl,-Bstatic -lpthread -lz -lpng -Wl,-Bdynamic -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -static-libstdc++ -static-libgcc -o ./build/voidsprite
+g++-13 ../freesprite/*.cpp ../freesprite/pugixml/*.cpp ../freesprite/zip/*.c ../freesprite/easybmp/*.cpp ../freesprite/astc_dec/*.cpp ../freesprite/liblcf/*.cpp ../freesprite/liblcf/generated/*.cpp -I../freesprite/liblcf -I../freesprite/liblcf/generated  -std=c++20 -Wl,-rpath,. -Wl,-Bstatic -lpthread -lz -lpng -Wl,-Bdynamic -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -static-libstdc++ -static-libgcc -o ./build/voidsprite
