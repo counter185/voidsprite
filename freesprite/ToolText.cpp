5478ca67dffbae9edf508962f76c7e1dc3efc015
counter185 <33550839+counter185@users.noreply.github.com>
2024-10-04 08:56:26 +0200

    text tool overhaul part 1

--- freesprite/ToolText.cpp
+++ freesprite/ToolText.cpp
@@ -2,24 +2,29 @@
 #include "PopupTextTool.h"
 #include "UITextField.h"
 #include "maineditor.h"
+#include "MainEditorPalettized.h"
+#include "Notification.h"
 
 void ToolText::clickPress(MainEditor* editor, XY pos)
 {
 	if (textSurface != NULL) {
-		uint8_t* pixels = (uint8_t*)malloc(textSurface->w * textSurface->h * 4);
+		uint8_t* pixels = (uint8_t*)textSurface->pixels;//(uint8_t*)malloc(textSurface->w * textSurface->h * 4);
 		int pitch = textSurface->w * 4;
-		SDL_ConvertPixels(textSurface->w, textSurface->h, SDL_PIXELFORMAT_ARGB8888, textSurface->pixels, textSurface->pitch, SDL_PIXELFORMAT_ARGB8888, pixels, pitch);
+		//SDL_ConvertPixels(textSurface->w, textSurface->h, SDL_PIXELFORMAT_ARGB8888, textSurface->pixels, textSurface->pitch, SDL_PIXELFORMAT_ARGB8888, pixels, pitch);
 		for (int y = 0; y < textSurface->h; y++) {
 			for (int x = 0; x < textSurface->w; x++) {
-				uint32_t* ppx = ((uint32_t*)pixels);
-				uint32_t pixel = *(ppx + (y * textSurface->w) + x);
+				//uint32_t* ppx = ((uint32_t*)pixels);
+				if (((uint32_t*)pixels)[y * textSurface->w + x] == 0) {
+					continue;
+				}
+				//uint32_t pixel = *(ppx + (y * textSurface->w) + x);
 				uint32_t colorModifier = editor->pickedColor;
-				pixel = (pixel & 0xff000000) | (colorModifier & 0xffffff);
+				//pixel = (pixel & 0xff000000) | (colorModifier & 0xffffff);
 
-				editor->SetPixel(XY{ pos.x + x, pos.y + y }, pixel);
+				editor->SetPixel(XY{ pos.x + x, pos.y + y }, editor->isPalettized ? ((MainEditorPalettized*)editor)->pickedPaletteIndex : editor->pickedColor);
 			}
 		}
-		free(pixels);
+		//free(pixels);
 	}
 }
 
@@ -73,9 +78,15 @@ void ToolText::renderText()
 	}
 
 	TTF_SetFontSize(font, textSize);
-	textSurface = TTF_RenderUTF8_Blended(font, text.c_str(), { 255, 255, 255, 255 });
+	textSurface = TTF_RenderUTF8_Solid(font, text.c_str(), { 255, 255, 255, 255 });
 	if (textSurface != NULL) {
+		SDL_Surface* converted = SDL_ConvertSurfaceFormat(textSurface, SDL_PIXELFORMAT_ARGB8888, 0);
+		SDL_FreeSurface(textSurface);
+		textSurface = converted;
 		cachedTextTexture = SDL_CreateTextureFromSurface(g_rd, textSurface);
+	}
+	else {
+		g_addNotification(ErrorNotification("Error", "Failed to render text"));
 	}
 
 }
