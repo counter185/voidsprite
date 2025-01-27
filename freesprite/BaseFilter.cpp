475b0f55a85cf25ff798ca31a2cefee3c234fdae
counter185 <33550839+counter185@users.noreply.github.com>
2025-01-27 21:07:43 +0100

    initial work on an image filter system

---
+++ freesprite/BaseFilter.cpp
@@ -0,0 +1,77 @@
+#include "BaseFilter.h"
+#include "Layer.h"
+
+Layer* BaseFilter::copy(Layer* src)
+{
+    if (src == NULL) {
+        return NULL;
+    }
+    Layer* ret = new Layer(src->w, src->h);
+    ret->name = src->name;
+    ret->colorKey = src->colorKey;
+    memcpy(ret->pixelData, src->pixelData, src->w * src->h * 4);
+    return ret;
+}
+
+Layer* FilterBlur::run(Layer* src, std::map<std::string, std::string> options)
+{
+    Layer* c = copy(src);
+    int radiusX = options.contains("radius.x") ? std::stod(options["radius.x"]) : 4;
+    int radiusY = options.contains("radius.y") ? std::stod(options["radius.y"]) : 4;
+    for (int y = 0; y < c->h; y++) {
+        for (int x = 0; x < c->w; x++) {
+
+            u32 pxnow = src->getPixelAt({ x,y }, true);
+            u64 r = pxnow & 0xFF;
+            u64 g = (pxnow >> 8) & 0xFF;
+            u64 b = (pxnow >> 16) & 0xFF;
+            u64 a = (pxnow >> 24) & 0xFF;
+            int measures = 1;
+
+            for (int rx = 1; rx < radiusX; rx++) {
+                if (x + rx < c->w) {
+					u32 px = src->getPixelAt({ x + rx,y }, true);
+					r += px & 0xFF;
+					g += (px >> 8) & 0xFF;
+					b += (px >> 16) & 0xFF;
+                    a += (px >> 24) & 0xFF;
+					measures++;
+				}
+				if (x - rx >= 0) {
+					u32 px = src->getPixelAt({ x - rx,y }, true);
+					r += px & 0xFF;
+					g += (px >> 8) & 0xFF;
+					b += (px >> 16) & 0xFF;
+					a += (px >> 24) & 0xFF;
+					measures++;
+				}
+            }
+
+            for (int ry = 1; ry < radiusY; ry++) {
+                if (y + ry < c->h) {
+                    u32 px = src->getPixelAt({ x,y + ry }, true);
+                    r += px & 0xFF;
+                    g += (px >> 8) & 0xFF;
+                    b += (px >> 16) & 0xFF;
+                    a += (px >> 24) & 0xFF;
+                    measures++;
+                }
+                if (y - ry >= 0) {
+					u32 px = src->getPixelAt({ x,y - ry }, true);
+					r += px & 0xFF;
+					g += (px >> 8) & 0xFF;
+					b += (px >> 16) & 0xFF;
+                    a += (px >> 24) & 0xFF;
+					measures++;
+				}
+            }
+
+            r /= measures;
+            g /= measures;
+            b /= measures;
+            a /= measures;
+            c->setPixel({ x,y }, PackRGBAtoARGB(r, g, b, a));
+        }
+    }
+    return c;
+}
---
+++ freesprite/BaseFilter.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "globals.h"
+
+#define INT_PARAM(name,min,max,def) (FilterParameter{name,min,max,def,PT_INT})
+#define FLOAT_PARAM(name,min,max,def) (FilterParameter{name,min,max,def,PT_FLOAT})
+#define COLORRGB_PARAM(name, def) (FilterParameter{name,min,max,def,PT_COLOR_RGB})
+#define COLORL_PARAM(name) (FilterParameter{name,0,255,127,PT_COLOR_L})
+
+enum ParameterType {
+	PT_INT = 0,
+	PT_FLOAT = 1,
+	PT_COLOR_RGB = 2,
+	PT_COLOR_L = 3,
+};
+
+struct FilterParameter {
+	std::string name;
+	double minValue = 0;
+	double maxValue = 1;
+	double defaultValue = 0.5;
+	ParameterType paramType;
+};
+
+class BaseFilter
+{
+public:
+	/// <summary>
+	/// Allocates a new layer, copies the source pixels to it and runs the filter on it.
+	/// </summary>
+	/// <param name="src">source layer to run the filter on</param>
+	/// <param name="options">options</param>
+	/// <returns>new layer</returns>
+	virtual Layer* run(Layer* src, std::map<std::string, std::string> options);
+	virtual std::vector<FilterParameter> getParameters() { return {}; }
+protected:
+	Layer* copy(Layer* src);
+};
+
+
+class FilterBlur : public BaseFilter
+{
+public:
+	Layer* run(Layer* src, std::map<std::string, std::string> options) override;
+	std::vector<FilterParameter> getParameters() override {
+		return {
+			INT_PARAM("radius.x", 1, 100, 4),
+			INT_PARAM("radius.y", 1, 100, 4),
+		};
+	}
+};
