1fffed256f94ec30cfd0d59adc5101f09dac228a
counter185 <33550839+counter185@users.noreply.github.com>
2025-03-20 16:04:52 +0100

    reference fit scaling mode

--- freesprite/PanelReference.cpp
+++ freesprite/PanelReference.cpp
@@ -1,86 +1,130 @@
 #include "PanelReference.h"
 #include "UIButton.h"
+#include "UIDropdown.h"
 
 PanelReference::PanelReference(Layer* t)
 {
-	previewTex = t;
+    previewTex = t;
+    c.dimensions = { previewTex->w, previewTex->h };
 
-	wxWidth = 400;
-	wxHeight = 300;
-
-	c.dimensions = { previewTex->w, previewTex->h };
-	c.recenter({wxWidth, wxHeight});
-
-	UIButton* closeBtn = new UIButton();
-	closeBtn->wxWidth = 30;
-	closeBtn->wxHeight = 20;
-	closeBtn->position = { wxWidth - 5 - closeBtn->wxWidth, 5 };
-	closeBtn->text = "X";
-	closeBtn->onClickCallback = [&](UIButton* caller) {
-		DrawableManager* wxs = getTopmostParent()->parentManager;
-		wxs->removeDrawable(getTopmostParent());
-	};
-	subWidgets.addDrawable(closeBtn);
+    initWidgets();
 }
 
 PanelReference::~PanelReference()
 {
-	if (previewTex != NULL) {
-		delete previewTex;
-	}
+    if (previewTex != NULL) {
+        delete previewTex;
+    }
 }
 
 bool PanelReference::isMouseIn(XY thisPositionOnScreen, XY mousePos)
 {
-	XY origin = xyAdd(thisPositionOnScreen, { 5,30 });
-	SDL_Rect canvasDraw = { origin.x, origin.y, wxWidth - 10, wxHeight - 40 };
-	return Panel::isMouseIn(thisPositionOnScreen, mousePos) || (enabled && pointInBox(mousePos, canvasDraw));
+    SDL_Rect canvasDraw = getCanvasDrawRect(thisPositionOnScreen);
+    return Panel::isMouseIn(thisPositionOnScreen, mousePos) || (enabled && pointInBox(mousePos, canvasDraw));
 }
 
 void PanelReference::handleInput(SDL_Event evt, XY gPosOffset)
 {
-	XY origin = xyAdd(gPosOffset, { 5,30 });
-	SDL_Rect canvasDraw = { origin.x, origin.y, wxWidth - 10, wxHeight - 40 };
-	if (evt.type == SDL_EVENT_MOUSE_BUTTON_DOWN && pointInBox({(int)evt.button.x, (int)evt.button.y}, canvasDraw)) {
-		dragging++;
-	}
-	else if (dragging > 0 && evt.type == SDL_EVENT_MOUSE_BUTTON_UP) {
-		dragging = 0;
-	}
-	else if (dragging && evt.type == SDL_EVENT_MOUSE_MOTION) {
-		c.panCanvas({ (int)evt.motion.xrel, (int)evt.motion.yrel });
-	}
-	else {
-		Panel::handleInput(evt, gPosOffset);
-	}
+    SDL_Rect canvasDraw = getCanvasDrawRect(gPosOffset);
+    if (currentMode == 0) {
+        if (evt.type == SDL_EVENT_MOUSE_BUTTON_DOWN && !subWidgets.mouseInAny(gPosOffset, { (int)evt.button.x, (int)evt.button.y }) && pointInBox({ (int)evt.button.x, (int)evt.button.y }, canvasDraw)) {
+            dragging++;
+        }
+        else if (dragging > 0 && evt.type == SDL_EVENT_MOUSE_BUTTON_UP) {
+            dragging = 0;
+        }
+        else if (dragging && evt.type == SDL_EVENT_MOUSE_MOTION) {
+            c.panCanvas({ (int)evt.motion.xrel, (int)evt.motion.yrel });
+        }
+        else {
+            Panel::handleInput(evt, gPosOffset);
+        }
+    }
+    else {
+        dragging = 0;
+        Panel::handleInput(evt, gPosOffset);
+    }
 }
 
 void PanelReference::render(XY at)
 {
-	if (!enabled) {
-		return;
-	}
-	
-	SDL_Rect panelRect = { at.x, at.y, wxWidth, wxHeight };
-	XY origin = xyAdd(at, { 5, 30 });
+    if (!enabled) {
+        return;
+    }
+    
+    SDL_Rect panelRect = { at.x, at.y, wxWidth, wxHeight };
 
-	renderGradient(panelRect, 0x80000000, 0x80000000, 0x80000000, 0x80404040);
-	
-	if (thisOrParentFocused()) {
-		SDL_SetRenderDrawColor(g_rd, 0xff, 0xff, 0xff, 255);
-		drawLine({ at.x, at.y }, { at.x, at.y + wxHeight }, XM1PW3P1(thisOrParentFocusTimer().percentElapsedTime(300)));
-		drawLine({ at.x, at.y }, { at.x + wxWidth, at.y }, XM1PW3P1(thisOrParentFocusTimer().percentElapsedTime(300)));
-	}
+    renderGradient(panelRect, 0x80000000, 0x80000000, 0x80000000, 0x80404040);
+    
+    if (thisOrParentFocused()) {
+        SDL_SetRenderDrawColor(g_rd, 0xff, 0xff, 0xff, 255);
+        drawLine({ at.x, at.y }, { at.x, at.y + wxHeight }, XM1PW3P1(thisOrParentFocusTimer().percentElapsedTime(300)));
+        drawLine({ at.x, at.y }, { at.x + wxWidth, at.y }, XM1PW3P1(thisOrParentFocusTimer().percentElapsedTime(300)));
+    }
 
-	SDL_Rect canvasDraw = { origin.x, origin.y, wxWidth - 10, wxHeight - 40 };
-	g_pushClip(canvasDraw);
-	c.lockToScreenBounds(0, 0, 0, 0, { canvasDraw.w, canvasDraw.h });
-	SDL_Rect texDraw = c.getCanvasOnScreenRect();
-	texDraw.x += at.x;
-	texDraw.y += at.y;
-	previewTex->render(texDraw);
-	g_popClip();
+    SDL_Rect canvasDraw = getCanvasDrawRect(at);
+    if (currentMode == 0) { //pixel-perfect
+        g_pushClip(canvasDraw);
+        c.lockToScreenBounds(0, 0, 0, 0, { canvasDraw.w, canvasDraw.h });
+        SDL_Rect texDraw = c.getCanvasOnScreenRect();
+        texDraw.x += at.x;
+        texDraw.y += at.y;
+        previewTex->render(texDraw);
+        g_popClip();
+    }
+    else if (currentMode == 1) {    //fit
+        previewTex->render(canvasDraw);
+    }
 
-	Panel::render(at);
+    Panel::render(at);
 
+}
+
+void PanelReference::eventDropdownItemSelected(int evt_id, int index, std::string name)
+{
+    if (evt_id == 0) {
+        currentMode = index;
+        initWidgets();
+    }
+}
+
+void PanelReference::initWidgets()
+{
+    subWidgets.freeAllDrawables();
+
+    if (currentMode == 0) { //pixel-perfect
+        wxWidth = 400;
+        wxHeight = 300;
+    }
+    else if (currentMode == 1) {  //fit
+        wxWidth = 400;
+        wxHeight = (int)(400.0 / previewTex->w * previewTex->h);
+    }
+
+    c.recenter({ wxWidth, wxHeight });
+
+    UIButton* closeBtn = new UIButton();
+    closeBtn->wxWidth = 30;
+    closeBtn->wxHeight = 20;
+    closeBtn->position = { wxWidth - 5 - closeBtn->wxWidth, 5 };
+    closeBtn->text = "X";
+    closeBtn->onClickCallback = [&](UIButton* caller) {
+        DrawableManager* wxs = getTopmostParent()->parentManager;
+        wxs->removeDrawable(getTopmostParent());
+        };
+    subWidgets.addDrawable(closeBtn);
+
+    std::vector<std::string> modes = {"Pixel-perfect", "Fit"};
+    UIDropdown* modeSwitch = new UIDropdown(modes);
+    modeSwitch->text = modes[currentMode];
+    modeSwitch->lastClick.start();
+    modeSwitch->wxWidth = 170;
+    modeSwitch->position = { wxWidth - 40 - modeSwitch->wxWidth, 1 };
+    modeSwitch->setCallbackListener(0, this);
+    subWidgets.addDrawable(modeSwitch);
+}
+
+SDL_Rect PanelReference::getCanvasDrawRect(XY at)
+{
+    return SDL_Rect{ at.x + 5, at.y + 35, wxWidth - 10, wxHeight - 40 };
 }
--- freesprite/PanelReference.h
+++ freesprite/PanelReference.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "Panel.h"
 #include "Canvas.h"
+#include "EventCallbackListener.h"
 
-class PanelReference : public Panel
+class PanelReference : public Panel, public EventCallbackListener
 {
 private:
     Layer* previewTex = NULL;
 public:
     Canvas c;
     int dragging = 0;
+
+    int currentMode = 0;
 
     PanelReference(Layer* t);
     ~PanelReference();
@@ -17,5 +20,10 @@ public:
     void handleInput(SDL_Event evt, XY gPosOffset) override;
 
     void render(XY at) override;
+
+    void eventDropdownItemSelected(int evt_id, int index, std::string name) override;
+
+    void initWidgets();
+    SDL_Rect getCanvasDrawRect(XY at);
 };
 
