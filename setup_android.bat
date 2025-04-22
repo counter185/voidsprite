3c3f3a4cb5be606a9c50dd914146dcbeed60b8bc
counter185 <33550839+counter185@users.noreply.github.com>
2025-04-22 06:52:04 +0200

    fix git clones

--- setup_android.bat
+++ setup_android.bat
@@ -4,12 +4,13 @@ rem   we need admin for symlinks for some reason
 net session > nul
 if not %errorlevel% == 0 goto noadmin
 
+cd %~dp0
 cd android-project\app\jni
 rem   todo: check if git is even present
-git clone https://github.com/libsdl-org/SDL.git
-git clone https://github.com/libsdl-org/SDL_image.git
-git clone https://github.com/libsdl-org/SDL_ttf.git
-git clone https://github.com/madler/zlib.git
+git clone --recursive https://github.com/libsdl-org/SDL.git
+git clone --recursive https://github.com/libsdl-org/SDL_image.git
+git clone --recursive https://github.com/libsdl-org/SDL_ttf.git
+git clone --recursive https://github.com/madler/zlib.git
 mklink /d liblcf "..\..\..\external_liblcf"
 cd src
 mklink /d voidsprite "..\..\..\..\freesprite"
