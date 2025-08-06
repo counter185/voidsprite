901033d06fab1e618a27dc6e8f79829e6d5bad44
counter185 <33550839+counter185@users.noreply.github.com>
2025-08-06 23:40:36 +0200

    android

--- setup_android.bat
+++ setup_android.bat
@@ -16,7 +16,9 @@ git checkout 520d73ae
 cd ..
 git clone --recursive https://github.com/libsdl-org/SDL_image.git
 git clone --recursive https://github.com/libsdl-org/SDL_ttf.git
-git clone --recursive https://github.com/libsdl-org/SDL_net.git
+REM   git clone --recursive https://github.com/libsdl-org/SDL_net.git
+REM   *use the above when SDL fixes android build
+git clone --recursive https://github.com/counter185/SDL_net-android21.git SDL_net
 git clone --recursive https://github.com/madler/zlib.git
 mklink /d liblcf "..\..\..\external_liblcf"
 cd src
--- setup_android_on_linux.sh
+++ setup_android_on_linux.sh
@@ -12,7 +12,8 @@ git checkout 520d73ae
 cd ..
 git clone --recursive https://github.com/libsdl-org/SDL_image.git
 git clone --recursive https://github.com/libsdl-org/SDL_ttf.git
-git clone --recursive https://github.com/libsdl-org/SDL_net.git
+#git clone --recursive https://github.com/libsdl-org/SDL_net.git
+git clone --recursive https://github.com/counter185/SDL_net-android21.git SDL_net
 git clone --recursive https://github.com/madler/zlib.git
 ln -s "../../../external_liblcf" liblcf 
 cd src
