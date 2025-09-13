d1c3540ae9ba04df781980cebece5ddf856fa9aa
counter185 <33550839+counter185@users.noreply.github.com>
2025-09-13 09:59:21 +0200

    cmake desktop build [no ci]

---
+++ cmake/setup_cmake.sh
@@ -0,0 +1,9 @@
+git clone --recursive https://github.com/libsdl-org/SDL.git
+git clone --recursive https://github.com/libsdl-org/SDL_image.git
+git clone --recursive https://github.com/libsdl-org/SDL_ttf.git
+git clone --recursive https://github.com/libsdl-org/SDL_net.git
+git clone --recursive https://github.com/madler/zlib.git
+git clone --recursive https://github.com/fmtlib/fmt.git
+ln -s "../external_liblcf" liblcf
+cd src
+ln -s "../../freesprite" voidsprite
