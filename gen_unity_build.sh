bb54e2443f2b6916543212eb36529e341e5b0815
counter185 <33550839+counter185@users.noreply.github.com>
2025-10-13 14:45:39 +0200

    Update gen_unity_build.sh

--- gen_unity_build.sh
+++ gen_unity_build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd freesprite
-find . -name "*.cpp" -exec echo "#include \"{}\"" >> "vsp_unity.cpp" \;
+find . -name "*.cpp" -exec echo "#include \"{}\"" >> "vsp_unity.txt" \;
+mv ./vsp_unity.txt ./vsp_unity.cpp
