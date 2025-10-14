86dd3dbd3e2c36ce38b3d30fc2aef528833540f7
buj <buj351@outlook.com>
2025-10-14 16:10:37 +0700

    made setup_cmake.sh not fail if directories already exist

--- cmake/setup_cmake.sh
+++ cmake/setup_cmake.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 cd "$(dirname "$(realpath "$0")")"
-git clone --recursive https://github.com/libsdl-org/SDL.git
-git clone --recursive https://github.com/libsdl-org/SDL_image.git
-git clone --recursive https://github.com/libsdl-org/SDL_ttf.git
-git clone --recursive https://github.com/libsdl-org/SDL_net.git
-git clone --recursive https://github.com/madler/zlib.git
-git clone --recursive https://github.com/fmtlib/fmt.git
-ln -s "../external_liblcf" liblcf
+if [ ! -d SDL ]; then git clone --recursive https://github.com/libsdl-org/SDL.git; fi
+if [ ! -d SDL_image ]; then git clone --recursive https://github.com/libsdl-org/SDL_image.git; fi
+if [ ! -d SDL_ttf ]; then git clone --recursive https://github.com/libsdl-org/SDL_ttf.git; fi
+if [ ! -d SDL_net ]; then git clone --recursive https://github.com/libsdl-org/SDL_net.git; fi
+if [ ! -d zlib ]; then git clone --recursive https://github.com/madler/zlib.git; fi
+if [ ! -d fmt ]; then git clone --recursive https://github.com/fmtlib/fmt.git; fi
+if [ ! -d liblcf ]; then ln -s "../external_liblcf" liblcf; fi
 cd src
-ln -s "../../freesprite" voidsprite
+if [ ! -d voidsprite ]; then ln -s "../../freesprite" voidsprite; fi
