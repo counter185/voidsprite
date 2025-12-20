c85dafd962175c6276bd5bacde74eab14437e406
counter185 <33550839+counter185@users.noreply.github.com>
2025-12-20 14:13:25 +0100

    yeah no wonder nothing was working

--- freesprite/platform.cpp
+++ freesprite/platform.cpp
@@ -4,15 +4,15 @@
 #include "FileIO.h"
 
 #if VSP_PLATFORM == VSP_PLATFORM_WIN32
-	#include "platform_windows.h"
+    #include "platform_windows.h"
 #elif VSP_PLATFORM == VSP_PLATFORM_MAC
-	#include "platform_macos.h"
+    #include "platform_macos.h"
 #elif VSP_PLATFORM == VSP_PLATFORM_VITA
-	#include "platform_vita.h"
+    #include "platform_vita.h"
 #elif VSP_PLATFORM == VSP_PLATFORM_ANDROID
     #include "platform_android.h"
 #elif VSP_PLATFORM == VSP_PLATFORM_EMSCRIPTEN
-	#include "platform_emscripten.h"
+    #include "platform_emscripten.h"
 #else
-	#include "platform_linux.h"
+    #include "platform_linux.h"
 #endif
