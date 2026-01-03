2dba29a975113d45cbb30f2382abdfe2095b0651
counter185 <33550839+counter185@users.noreply.github.com>
2026-01-03 12:20:58 +0100

    sdl_image decided to go mega silly and break browser

--- cmake/setup_cmake.sh
+++ cmake/setup_cmake.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 cd "$(dirname "$(realpath "$0")")"
 if [ ! -d SDL ]; then git clone --recursive https://github.com/libsdl-org/SDL.git; fi
-if [ ! -d SDL_image ]; then git clone --recursive https://github.com/libsdl-org/SDL_image.git; fi
+if [ ! -d SDL_image ]; then 
+	git clone --recursive https://github.com/libsdl-org/SDL_image.git; 
+	cd SDL_image
+	git checkout 9b3bddc
+	cd ..
+fi
 if [ ! -d SDL_ttf ]; then git clone --recursive https://github.com/libsdl-org/SDL_ttf.git; fi
 if [ ! -d SDL_net ]; then git clone --recursive https://github.com/libsdl-org/SDL_net.git; fi
 if [ ! -d zlib ]; then git clone --recursive https://github.com/madler/zlib.git; fi
