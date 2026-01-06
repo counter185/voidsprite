ffbacbb7e230c2956d8321a40094d9a3568c25dc
counter185 <33550839+counter185@users.noreply.github.com>
2026-01-06 15:20:05 +0100

    fixx android

--- setup_android_on_linux.sh
+++ setup_android_on_linux.sh
@@ -18,6 +18,9 @@ git clone --recursive https://github.com/libsdl-org/SDL_ttf.git
 #git clone --recursive https://github.com/libsdl-org/SDL_net.git
 git clone --recursive https://github.com/counter185/SDL_net-android21.git SDL_net
 git clone --recursive https://github.com/madler/zlib.git
+cd zlib
+git checkout v1.3.1.2
+cd ..
 ln -s "../../../external_liblcf" liblcf 
 cd src
 ln -s "../../../../freesprite" voidsprite
