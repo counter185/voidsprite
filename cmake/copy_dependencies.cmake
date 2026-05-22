ee419368b6dd482bbae8fe37a14f657657c3ca5b
counter185 <33550839+counter185@users.noreply.github.com>
2026-05-22 18:15:30 +0200

    Update copy_dependencies.cmake

--- cmake/copy_dependencies.cmake
+++ cmake/copy_dependencies.cmake
@@ -1,6 +1,6 @@
 function (copy_rt_libs binary_dir)
     file(GET_RUNTIME_DEPENDENCIES RESOLVED_DEPENDENCIES_VAR rtlibs EXECUTABLES "${binary_dir}/voidsprite")
-    file(COPY ${rtlibs} DESTINATION "${binary_dir}")
+    file(COPY ${rtlibs} DESTINATION "${binary_dir}" FOLLOW_SYMLINK_CHAIN)
 endfunction()
 
 copy_rt_libs(${BINARY_DIR})
